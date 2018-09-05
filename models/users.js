b15c63be83c35056163c4feb433eac5d740b72b8
cyrsis <em3888@gmail.com>
2018-09-05 18:38:58 +0800

    laterest work on the models

---
+++ models/users.js
@@ -0,0 +1,21 @@
+const mongoose = require(mongoose);
+
+const Schema = mongoose.Schema
+
+
+let userSchema = new Schema(
+    {
+        first: {type: String, required: true},
+        last: {type: String, required: true},
+        email: {type: String, required: true},
+        password: {type: String, required: true},
+        isActive: {type: Boolean, required: true},
+        createdOn: {type: Date, required: true},
+
+
+    }
+)
+
+const User = mongoose.mode('User', userSchema);
+
+module.exports = User
